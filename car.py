b25b297e0b41b4d4bf043c404480c01cbb1506d0
Ricky Surya Adiputra <84176709+rickysurya@users.noreply.github.com>
2023-01-05 23:45:00 +0700

    Delete car.py

--- car.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC, abstractmethod
-from serviceable import Serviceable
-
-class Car(Serviceable, ABC):
-    def __init__(self, engine, battery):
-        self.engine = engine
-        self.battery = battery
