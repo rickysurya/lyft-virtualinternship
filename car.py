b9c78b694d088276b7b3a9a5dd3bf1493a647c6b
Ricky Surya Adiputra <84176709+rickysurya@users.noreply.github.com>
2023-01-05 23:10:55 +0700

    initial commit

---
+++ car.py
@@ -0,0 +1,7 @@
+from abc import ABC, abstractmethod
+from serviceable import Serviceable
+
+class Car(Serviceable, ABC):
+    def __init__(self, engine, battery):
+        self.engine = engine
+        self.battery = battery
