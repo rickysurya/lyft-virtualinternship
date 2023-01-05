e3985c7d50bcc13541c84fd76b3a87b0be6b80a0
Ricky Surya Adiputra <84176709+rickysurya@users.noreply.github.com>
2023-01-05 23:45:13 +0700

    Delete serviceable.py

--- serviceable.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-
-class Serviceable(ABC):
-    def __init__(self) -> None :
-        pass
-
-    @abstractmethod
-    def need_service(self) ->bool:
-        pass
